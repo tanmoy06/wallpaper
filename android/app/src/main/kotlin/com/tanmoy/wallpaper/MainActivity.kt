8cf48c925366964ecc30048643bbe9547b949d9d
tanmoy06 <iamtanmoysarkar007@gmail.com>
2024-12-11 03:44:31 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/tanmoy/wallpaper/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tanmoy.wallpaper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: null, // No logic, button disabled
+              child: Text('Login'),
+            ),
+            TextButton(
+              onPressed: null, // No logic, button disabled
+              child: Text('Forgot Password?'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
